7aaf0040d42e9cfd2b3653ff07d46a6cb05c1b3b
Onat Inak <onat.inak@sima.ai>
2026-06-30 22:08:36 +0200

    fix(segmenter): seed video graph with ev74 tensor

--- examples/segmentation/single-stream-instance-segmenter/src/cpp/main.cpp
+++ examples/segmentation/single-stream-instance-segmenter/src/cpp/main.cpp
@@ -597,7 +597,9 @@ PipelineRuntime build_pipeline(const AppConfig& cfg) {
   runtime.video_graph.add(simaai::neat::nodes::Input(video_input));
   runtime.video_graph.add(simaai::neat::nodes::groups::VideoSender(video_options));
   cv::Mat seed(runtime.frame_h, runtime.frame_w, CV_8UC3, cv::Scalar(0, 0, 0));
-  runtime.video_run = runtime.video_graph.build(std::vector<cv::Mat>{seed});
+  const auto seed_tensor = simaai::neat::Tensor::from_cv_mat(
+      seed, simaai::neat::ImageSpec::PixelFormat::RGB, simaai::neat::TensorMemory::EV74);
+  runtime.video_run = runtime.video_graph.build(simaai::neat::TensorList{seed_tensor});
 
   simaai::neat::MetadataSenderOptions metadata_options;
   metadata_options.host = cfg.insight_host;
